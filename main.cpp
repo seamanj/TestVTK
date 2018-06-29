fd7b673b964eb40727fc16d68339a483950cdfb1
tjiang <tjiang@bournemouth.ac.uk>
2018-06-29 07:18:32 +0100

    Step1 - A "Hello World" style example of a simple visualization pipeline

--- main.cpp
+++ main.cpp
@@ -1,9 +1,45 @@
-#include <iostream>
+#include "vtkConeSource.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkRenderWindow.h"
+#include "vtkCamera.h"
+#include "vtkActor.h"
+#include "vtkRenderer.h"
 
-using namespace std;
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    vtkConeSource *cone = vtkConeSource::New();
+    cone->SetHeight(3.0);
+    cone->SetRadius(1.0);
+    cone->SetResolution(10);
+
+    vtkPolyDataMapper *coneMapper = vtkPolyDataMapper::New();
+    coneMapper->SetInputConnection(cone->GetOutputPort());
+
+    vtkActor *coneActor = vtkActor::New();
+    coneActor->SetMapper(coneMapper);
+
+    vtkRenderer *ren1 = vtkRenderer::New();
+    ren1->AddActor(coneActor);
+    ren1->SetBackground(0.1, 0.2, 0.4);
+
+
+    vtkRenderWindow *renWin = vtkRenderWindow::New();
+    renWin->AddRenderer(ren1);
+    renWin->SetSize(300, 300);
+
+    int i;
+    for(int i = 0; i < 360; ++i)
+    {
+        renWin->Render();
+        ren1->GetActiveCamera()->Azimuth(1);
+    }
+
+    cone->Delete();
+    coneMapper->Delete();
+    coneActor->Delete();
+    ren1->Delete();
+    renWin->Delete();
+
 }
