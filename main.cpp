aefa3b3b4c9af1ddcb6eab8cd88d28edbc54c5f0
tjiang <tjiang@bournemouth.ac.uk>
2018-07-02 09:02:21 +0100

    Step4 - Modifying properties and transformations

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "vtkActor.h"
 #include "vtkRenderer.h"
 #include "vtkCommand.h"
-
+#include "vtkProperty.h"
 
 
 
@@ -22,38 +22,49 @@ int main()
 
     vtkActor *coneActor = vtkActor::New();
     coneActor->SetMapper(coneMapper);
+    coneActor->GetProperty()->SetColor(0.2, 0.63, 0.79);
+    coneActor->GetProperty()->SetDiffuse(0.7);
+    coneActor->GetProperty()->SetSpecular(0.4);
+    coneActor->GetProperty()->SetSpecularPower(20);
+
+
+    vtkProperty *property = vtkProperty::New();
+    property->SetColor(1.0, 0.3882, 0.2784);
+    property->SetDiffuse(0.7);
+    property->SetSpecular(0.4);
+    property->SetSpecularPower(20);
+
+    vtkActor *coneActor2 = vtkActor::New();
+    coneActor2->SetMapper(coneMapper);
+    coneActor2->GetProperty()->SetColor(0.2, 0.63, 0.79);
+    coneActor2->SetProperty(property);
+    coneActor2->SetPosition(0, 2, 0);
 
     vtkRenderer *ren1= vtkRenderer::New();
     ren1->AddActor( coneActor );
+    ren1->AddActor( coneActor2 );
     ren1->SetBackground( 0.1, 0.2, 0.4 );
-    ren1->SetViewport(0.0, 0.0, 0.5, 1.0);
 
-    vtkRenderer *ren2= vtkRenderer::New();
-    ren2->AddActor( coneActor );
-    ren2->SetBackground( 0.2, 0.3, 0.5 );
-    ren2->SetViewport(0.5, 0.0, 1.0, 1.0);
+
 
     vtkRenderWindow *renWin = vtkRenderWindow::New();
     renWin->AddRenderer(ren1);
-    renWin->AddRenderer( ren2 );
-    renWin->SetSize( 600, 300 );
+    renWin->SetSize( 300, 300 );
 
-    ren1->ResetCamera();
-    ren1->GetActiveCamera()->Azimuth(90);
 
     int i;
     for(int i = 0; i < 360; ++i)
     {
         renWin->Render();
         ren1->GetActiveCamera()->Azimuth(1);
-        ren2->GetActiveCamera()->Azimuth( 1 );
     }
 
     cone->Delete();
     coneMapper->Delete();
     coneActor->Delete();
+    property->Delete();
+    coneActor2->Delete();
     ren1->Delete();
-    ren2->Delete();
     renWin->Delete();
 
 }
