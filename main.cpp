c3d452ba436101100d2ae0a37ce4358ad9660a8c
tjiang <tjiang@bournemouth.ac.uk>
2018-07-02 09:14:55 +0100

    Step5 - Specifying a particular interaction style

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "vtkConeSource.h"
 #include "vtkPolyDataMapper.h"
 #include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
 #include "vtkCamera.h"
 #include "vtkActor.h"
 #include "vtkRenderer.h"
-#include "vtkCommand.h"
-#include "vtkProperty.h"
+#include "vtkInteractorStyleTrackballCamera.h"
 
 
 
@@ -22,27 +22,10 @@ int main()
 
     vtkActor *coneActor = vtkActor::New();
     coneActor->SetMapper(coneMapper);
-    coneActor->GetProperty()->SetColor(0.2, 0.63, 0.79);
-    coneActor->GetProperty()->SetDiffuse(0.7);
-    coneActor->GetProperty()->SetSpecular(0.4);
-    coneActor->GetProperty()->SetSpecularPower(20);
 
-
-    vtkProperty *property = vtkProperty::New();
-    property->SetColor(1.0, 0.3882, 0.2784);
-    property->SetDiffuse(0.7);
-    property->SetSpecular(0.4);
-    property->SetSpecularPower(20);
-
-    vtkActor *coneActor2 = vtkActor::New();
-    coneActor2->SetMapper(coneMapper);
-    coneActor2->GetProperty()->SetColor(0.2, 0.63, 0.79);
-    coneActor2->SetProperty(property);
-    coneActor2->SetPosition(0, 2, 0);
 
     vtkRenderer *ren1= vtkRenderer::New();
     ren1->AddActor( coneActor );
-    ren1->AddActor( coneActor2 );
     ren1->SetBackground( 0.1, 0.2, 0.4 );
 
 
@@ -51,20 +34,24 @@ int main()
     renWin->AddRenderer(ren1);
     renWin->SetSize( 300, 300 );
 
+    vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
+    iren->SetRenderWindow(renWin);
 
-    int i;
-    for(int i = 0; i < 360; ++i)
-    {
-        renWin->Render();
-        ren1->GetActiveCamera()->Azimuth(1);
-    }
+    vtkInteractorStyleTrackballCamera *style =
+      vtkInteractorStyleTrackballCamera::New();
+    iren->SetInteractorStyle(style);
+
+    iren->Initialize();
+    iren->Start();
+
 
     cone->Delete();
     coneMapper->Delete();
     coneActor->Delete();
-    property->Delete();
-    coneActor2->Delete();
     ren1->Delete();
     renWin->Delete();
+    iren->Delete();
+    style->Delete();
 
+    return 0;
 }
